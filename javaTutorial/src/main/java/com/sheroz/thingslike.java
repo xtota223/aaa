f4cd75d7d355e0021a1bfbfbb71218621c81ee6b
xtota223 <63666253+xtota223@users.noreply.github.com>
2020-05-15 23:08:13 -0400

    Add files via upload

---
+++ javaTutorial/src/main/java/com/sheroz/thingslike.java
@@ -0,0 +1,10 @@
+package com.sheroz;
+
+public class thingslike {
+    public static void main(String args[])   //error, idk how to fix it
+          System.out.println ("chocolate, royalties, sleep");    //one more error
+    {
+        MySalary = 1_000_000.00;      //and one more, idk it's too hard
+    }
+
+}
